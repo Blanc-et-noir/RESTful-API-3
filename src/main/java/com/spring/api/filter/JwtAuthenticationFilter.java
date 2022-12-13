7724fcb830c2edc31a97955711b15eb3e32ee681
Blanc_et_Noir <dign9060@gmail.com>
2022-12-13 23:47:18 +0900

    필요없는 주석 제거

--- src/main/java/com/spring/api/filter/JwtAuthenticationFilter.java
+++ src/main/java/com/spring/api/filter/JwtAuthenticationFilter.java
@@ -39,20 +39,16 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter{
 
         if(!StringUtils.hasText(user_accesstoken)) {
         	request.setAttribute("customException", new CustomException(AuthError.NOT_FOUND_USER_ACCESSTOKEN));
-        	//throw new CustomException(AuthError.NOT_FOUND_USER_ACCESSTOKEN);
         }else if(!jwtTokenProvider.validateToken(user_accesstoken)) {
         	request.setAttribute("customException", new CustomException(AuthError.INVALID_USER_ACCESSTOKEN));
-        	//throw new CustomException(AuthError.INVALID_USER_ACCESSTOKEN);
         }else {
             String user_id = jwtTokenProvider.getUserIdFromJWT(user_accesstoken);
             String user_role = jwtTokenProvider.getUserRoleFromJWT(user_accesstoken);
             
             if(!jwtTokenProvider.getTokenType(user_accesstoken).equals("user_accesstoken")) {
             	request.setAttribute("customException", new CustomException(AuthError.INVALID_USER_ACCESSTOKEN));
-            	//throw new CustomException(AuthError.INVALID_USER_ACCESSTOKEN);
             }else if(redisUtil.getData(user_accesstoken)!=null){
             	request.setAttribute("customException", new CustomException(AuthError.IS_LOGGED_OUT_ACCESSTOKEN));
-            	//throw new CustomException(AuthError.IS_LOGGED_OUT_ACCESSTOKEN);
             }else {
                 UserAuthentication authentication = new UserAuthentication(user_id, null, Arrays.asList(new SimpleGrantedAuthority(user_role)));
                 authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
